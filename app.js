be64dcd99f81b78d7483080f0abcbaa48ce6c61a
L0RDTEM4S <thiagoxfalleryt@gmail.com>
2025-10-29 03:18:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ var CONFIG = {
     enabled: true,
     list: [
       'asset://garrysmod/sound/loadscreen/music.wav',
-      'asset://garrysmod/sound/loadscreen/tema2.wav',
+      // 'asset://garrysmod/sound/loadscreen/tema2.wav',
       // 'asset://garrysmod/sound/loadscreen/tema3.wav'
     ],
     src: 'asset://garrysmod/sound/loadscreen/music.wav',
@@ -451,6 +451,7 @@ window.onGMODTick = function (data) {
   window.__QP_SESSION_SEC__ = t; // ← guardamos sesión
   updateTimeStat((window.__QP_BASE_TOTAL__ || 0) + t); // ← base persistente + sesión
 };
+
 
 
 
